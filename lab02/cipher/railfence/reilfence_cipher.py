42eb913cbce4475b0aab0d235c2b2af7cc587919
phatphat292 <phatpham581@gmail.com>
2025-05-28 00:02:20 +0700

    [add] lab02

--- lab02/cipher/railfence/reilfence_cipher.py
+++ lab02/cipher/railfence/reilfence_cipher.py
@@ -1,23 +1,31 @@
 class RailFenceCipher:
     def __init__(self):
         pass
+
     def rail_fence_encrypt(self, plain_text, num_rails):
-        rails = [[]for _ in range(num_rails)]
+        rails = [[] for _ in range(num_rails)]
         rail_index = 0
-        direction =1
+        direction = 1
+
         for char in plain_text:
             rails[rail_index].append(char)
+
             if rail_index == 0:
                 direction = 1
-            elif rail_index == num_rails -1:
+            elif rail_index == num_rails - 1:
                 direction = -1
-                rail_index += direction
+
+            rail_index += direction  # ✅ Đặt ngoài điều kiện
+
         cipher_text = ''.join(''.join(rail) for rail in rails)
         return cipher_text
-    def rail_fence_decrypt(self, plain_text, num_rails):
+
+    def rail_fence_decrypt(self, cipher_text, num_rails):
         rails_lengths = [0] * num_rails
         rail_index = 0
         direction = 1
+
+        # Tính độ dài từng rail
         for _ in range(len(cipher_text)):
             rails_lengths[rail_index] += 1
             if rail_index == 0:
@@ -25,16 +33,25 @@ class RailFenceCipher:
             elif rail_index == num_rails - 1:
                 direction = -1
             rail_index += direction
+
+        # Chia cipher_text vào các rail
         rails = []
         start = 0
         for length in rails_lengths:
-            rail.append(cipher_text[start:start + length])
+            rails.append(cipher_text[start:start + length])
             start += length
-        plain_text += rails[rail_index][0]
-        rails[rail_index] = rails[rail_index][1:]
-        if rail_index == 0:
-            direction = 1
-        elif rail_index == num_rails - 1:
-            direction = -1
-        rail_index += direction
-    return plain_text
+
+        # Lấy lại văn bản gốc từ các rail
+        plain_text = ""
+        rail_index = 0
+        direction = 1
+        for _ in range(len(cipher_text)):
+            plain_text += rails[rail_index][0]
+            rails[rail_index] = rails[rail_index][1:]
+            if rail_index == 0:
+                direction = 1
+            elif rail_index == num_rails - 1:
+                direction = -1
+            rail_index += direction
+
+        return plain_text
